bde917d8abefc91f9711ccf0606384df8d3d29c8
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-06-03 23:10:39 -0400

    Push latest cache additions

--- aiogtrans/cache.py
+++ aiogtrans/cache.py
@@ -13,3 +13,46 @@ furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 """
+
+from collections import OrderedDict
+from models import Translated, Detected
+import typing
+
+
+class Cache:
+    """
+    LRU based cache to store api calls, 2 will usually be created, one for translations, and one for detections, respectively
+    """
+    
+    def __init__(self, capacity: int = 1000) -> None:
+        """
+        Cache Init
+        
+        Parameters
+        ----------
+        capacity: int
+            The amount of items to be stored in the cache
+            Default 1,000
+            
+        """
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> typing.Union[Translated, Detected]:
+        """
+        Retrieve a key
+        """
+        if key not in self.cache:
+            return -1
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+    def add(self, key: int, value: int) -> None:
+        """
+        Add a key and value to the cache
+        """
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last = False)
---
+++ docs/source/aiohttptranslator.rst
@@ -0,0 +1,4 @@
+AiohttpTranslator
+=================
+
+something
